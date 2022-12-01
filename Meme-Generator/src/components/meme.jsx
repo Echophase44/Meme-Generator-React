b11622d9453d738688b00e0ee1dfd524c853132b
Nick <Echophase44@github.com>
2022-12-01 16:07:33 -0500

    Finished project utilizing API instead of Data

--- Meme-Generator/src/components/meme.jsx
+++ Meme-Generator/src/components/meme.jsx
@@ -10,17 +10,32 @@ function Meme() {
   })
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}, [])
   
   
   function getMemeImage() {
       const memesArray = allMemeImages.data.memes
       const randomNumber = Math.floor(Math.random() * memesArray.length)
       const url = memesArray[randomNumber].url
+      
       setMeme(prevMeme => ({
           ...prevMeme,
           randomImage: url
       }))
       
+  }
+
+  function handleChange(event){
+    const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
   }
   
     return (
@@ -30,11 +45,17 @@ function Meme() {
                       type="text"
                       placeholder="Top text"
                       className="form--input"
+                      onChange={handleChange}
+                      value={meme.topText}
+                      name="topText"
                   />
                   <input 
                       type="text"
                       placeholder="Bottom text"
                       className="form--input"
+                      onChange={handleChange}
+                      name="bottomText"
+                      value={meme.bottomText}
                   />
                   <button
                       type = "button" 
@@ -44,9 +65,13 @@ function Meme() {
                       Get a new meme image
                   </button>
               </form>
-              <img src={meme.randomImage} alt="Meme" className="meme--image"/>
+              <div className="meme">
+                <img src={meme.randomImage} alt="Meme" className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+              </div>
       </main>
     )
   }
-  
+
 export default Meme
